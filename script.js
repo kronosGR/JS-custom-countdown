14a704ce53d402a7fed6f8174ba0e23b475c7225
kronosGR <geo.elgeo@gmail.com>
2022-01-17 20:01:42 +0100

    added countdown calculations

--- script.js
+++ script.js
@@ -2,16 +2,49 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
-let countdownDate ='';
+let countdownDate = '';
+let countDownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 const today = new Date().toISOString().split('T')[0];
-dateEl.setAttribute('min', today)
+dateEl.setAttribute('min', today);
+
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countDownValue - now;
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`
+  timeElements[1].textContent = `${hours}`
+  timeElements[2].textContent = `${minutes}`
+  timeElements[3].textContent = `${seconds}`
+
+  inputContainer.hidden = true;
+  countdownEl.hidden = false;
+}
 
 function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
+
+  countDownValue = new Date(countdownDate).getTime();
+  updateDOM();
 }
 
-countdownForm.addEventListener('submit',updateCountdown)
+countdownForm.addEventListener('submit', updateCountdown);
